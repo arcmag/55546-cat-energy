835f22177694cbbb78ef44ba0c58f3b625847b7e
Николай <arcmag-work@mail.ru>
2018-10-16 19:16:39 +0700

    Исправление SVG спрайта, небольшие правки по JS

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,5 @@
-document.querySelector(".main-nav--no-js").classList.remove("main-nav--no-js")
+document.querySelector(".main-nav--no-js").classList.remove("main-nav--no-js");
+
+document.querySelector(".main-nav__btn").addEventListener("click", function () {
+  document.querySelector(".main-nav__list").classList.toggle("main-nav__list--hidden");
+});
